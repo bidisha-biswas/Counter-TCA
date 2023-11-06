7cff7857ffb129796a96fcf306fc3afc63feb839
Bidisha Biswas <bidisha.biswas@ibotta.com>
2023-11-05 20:53:48 -0700

    Initial Commit

---
+++ Counter-TCA/Counter_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_TCAApp.swift
+//  Counter-TCA
+//
+//  Created by Bidisha Biswas on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Counter_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
